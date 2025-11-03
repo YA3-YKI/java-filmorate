ffd578701d35004fb8aad05021a70956d84cd918
Sergej <serb0jko@yandex.ru>
2025-11-03 17:38:24 +0700

    [Sprint 11] Реализовать ТЗ из ФЗ 11]

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -0,0 +1,11 @@
+package ru.yandex.practicum.filmorate.storage.film;
+
+import ru.yandex.practicum.filmorate.model.Film;
+import java.util.Collection;
+
+public interface FilmStorage {
+    Film add(Film film);
+    Film update(Film film);
+    Film getById(Long id);
+    Collection<Film> getAll();
+}
